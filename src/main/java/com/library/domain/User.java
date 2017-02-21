d4904b5138ee84c3b45aa01fb30f28db58cb45b3
alex <mymail@somewhere.com>
2017-02-22 00:18:16 +0200

    added enteties

---
+++ src/main/java/com/library/domain/User.java
@@ -0,0 +1,53 @@
+package com.library.domain;
+
+import java.util.List;
+
+public class User {
+
+    private long id;
+    private String name;
+    private int age;
+    private List<Book> bookList;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", bookList=" + bookList +
+                '}';
+    }
+}
